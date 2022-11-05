9fb6d5866f10cc2ecad0fe0cd198f7dea283b186
cma124 <chan.myae.a.099@gmail.com>
2022-11-05 19:31:50 +0630

    update dark mode with local storage

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,38 @@
+let theme = localStorage.getItem('portfolio-theme');
+
 $(document).ready(function () {
     $('#img-sun').hide();
 
+    if (theme === 'dark') {
+        toggleTheme();
+    }
+
     $('#btn-mode').click(function () {
-        $('#img-sun').toggle();
-        $('#img-moon').toggle();
+        theme = theme === 'dark' ? null : 'dark';
+        localStorage.setItem('portfolio-theme', theme);
 
-        $('section').toggleClass('bg-dark text-white');
-        $('section a').toggleClass('text-info');
-
-        $('.skill').toggleClass('bg-white');
-        $('.skill').toggleClass('bg-black');
-
-        $('.card-body').toggleClass('bg-black');
-        $('.card-body').toggleClass('text-white');
-
-        $('.accordion-body').toggleClass('bg-black text-white');
-
-        $('.footer').toggleClass('bg-dark text-white');
-        $('.footer a').toggleClass('text-info');
+        toggleTheme();
     });
 
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
+
+function toggleTheme() {
+    $('#img-sun').toggle();
+    $('#img-moon').toggle();
+
+    $('section').toggleClass('bg-dark text-white');
+    $('section a').toggleClass('text-info');
+
+    $('.skill').toggleClass('bg-white');
+    $('.skill').toggleClass('bg-black');
+
+    $('.card-body').toggleClass('bg-black');
+    $('.card-body').toggleClass('text-white');
+
+    $('.accordion-body').toggleClass('bg-black text-white');
+
+    $('.footer').toggleClass('bg-dark text-white');
+    $('.footer a').toggleClass('text-info');
+}
